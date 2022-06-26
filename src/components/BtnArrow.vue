10af9d1a142b641f7e7d9bb89855fe12373c30b8
PamelaPz <jpamelapz02@gmail.com>
2022-06-26 14:49:58 -0500

    componentes

---
+++ src/components/BtnArrow.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="btn-arrow" :class="color">
+    <p class="btn-arrow-text">
+      {{ msg }}
+      <span class="btn-arrow-icon" :class="icon">
+        <img src="../assets/imgs/flechaBtn.svg" alt="" />
+      </span>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+    color: String,
+    icon: String,
+  },
+};
+</script>
---
+++ src/components/Footer.vue
@@ -0,0 +1,9 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: "w-footer",
+};
+</script>
